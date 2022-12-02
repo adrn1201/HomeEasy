5cfd2e1d435727d4ea22e4a74b0fc0c80ba4d52f
Carlos Adrian Catindig <adriancatindig1216@gmail.com>
2022-12-02 17:09:07 +0800

    Set Data for Firebase Nodes

--- app/src/main/java/com/example/homeeasy/MainActivity.kt
+++ app/src/main/java/com/example/homeeasy/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.homeeasy
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 import com.example.homeeasy.databinding.ActivityMainBinding
 import com.google.firebase.database.*
@@ -10,7 +11,9 @@ import com.google.firebase.database.*
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var database: DatabaseReference
-
+    private lateinit var textMsg: String
+    private lateinit var textColor: String
+    private lateinit var toastMsg: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -19,7 +22,94 @@ class MainActivity : AppCompatActivity() {
         if (supportActionBar != null) {
             supportActionBar!!.hide()
         }
+        binding.securitySwitch.setOnCheckedChangeListener { _, isChecked ->
+            setData(
+                isChecked,
+                "secured",
+                "Enabled",
+                "Security is Enabled",
+                "Disabled",
+                "Security is Disabled",
+                "FAILED to set new Security Value",
+                binding.securityIndicator
+            )
+        }
+        binding.doorSwitch.setOnCheckedChangeListener { _, isChecked ->
+            setData(
+                isChecked,
+                "doorLocked",
+                "Locked",
+                "Door is Locked",
+                "Unlocked",
+                "Door is Unlocked",
+                "FAILED to set new Door Lock Value",
+                binding.doorIndicator
+            )
+        }
+        binding.sensorSwitch.setOnCheckedChangeListener { _, isChecked ->
+            setData(
+                isChecked,
+                "ldrEnabled",
+                "Enabled",
+                "Sensor is Enabled",
+                "Disabled",
+                "Sensor is Disabled",
+                "FAILED to set new Sensor Value",
+                binding.sensorIndicator
+            )
+        }
         databaseListener()
+    }
+
+    private fun setData(
+        isChecked: Boolean,
+        childNode:String,
+        checkedTxtMsg:String,
+        checkedToastMsg:String,
+        unCheckedTxtMsg:String,
+        unCheckedToastMsg:String,
+        failureMsg:String,
+        txtView: TextView,
+        checkedTxtColor:String="#00FF00",
+        unCheckedTxtColor:String="#FF0000",
+    ) {
+        database = FirebaseDatabase.getInstance().getReference("componentStatus")
+        database.child(childNode).setValue(isChecked).addOnSuccessListener {
+            validateSwitchStatus(
+                isChecked,
+                checkedTxtMsg,
+                checkedTxtColor,
+                checkedToastMsg,
+                unCheckedTxtMsg,
+                unCheckedTxtColor,
+                unCheckedToastMsg
+            )
+            txtView.text = textMsg
+            txtView.setTextColor(Color.parseColor(textColor))
+            Toast.makeText(this, toastMsg, Toast.LENGTH_SHORT).show()
+        }.addOnFailureListener {
+            Toast.makeText(this, failureMsg  , Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun validateSwitchStatus(
+        isChecked: Boolean,
+        checkedTxtMsg: String,
+        checkedTxtColor: String,
+        checkedToastMsg: String,
+        unCheckedTxtMsg: String,
+        unCheckedTxtColor: String,
+        unCheckedToastMsg: String
+    ) {
+        if (isChecked) {
+            textMsg = checkedTxtMsg
+            textColor = checkedTxtColor
+            toastMsg = checkedToastMsg
+        } else {
+            textMsg = unCheckedTxtMsg
+            textColor = unCheckedTxtColor
+            toastMsg = unCheckedToastMsg
+        }
     }
 
     private fun databaseListener() {
